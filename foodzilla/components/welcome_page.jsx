4b26dd60344ac0a4bdf13e8d36452beb993956c3
flamingosolid <josefabdo90@gmail.com>
2023-05-12 13:12:08 +0200

    first two welcomepages

--- foodzilla/components/welcome_page.jsx
+++ foodzilla/components/welcome_page.jsx
@@ -4,25 +4,35 @@ import global from "../styles/global.scss";
 import React from "react";
 import { useState } from "react";
 
-function WelcomePage({
-  logo,
-  logoAlt,
-  welcomeText,
-  btnImg,
-  btnAlt,
-  handleClick,
-}) {
+function WelcomePage({ logo, logoAlt, btnImg, btnAlt }) {
+  const [introPage, setIntroPage] = useState(false);
+  const [getStarted, setGetStarted] = useState(false);
   return (
     <>
       <section className="welcome">
         <div className="logo">
           <img src={logo} alt={logoAlt} />
         </div>
-        <div className="welcome-text">
-          <h3>{welcomeText}</h3>
-        </div>
+        {!getStarted ? (
+          <div className="welcome-text">
+            {!introPage ? (
+              <h4>
+                Your cooking is worth saving. Get organized with foodzilla
+              </h4>
+            ) : (
+              <h4>
+                Create your personal recepie liberary. Share recepies with
+                friends. Save your favorites
+              </h4>
+            )}
+          </div>
+        ) : null}
         <div className="next-btn">
-          <img onClick={handleClick} src={btnImg} alt={btnAlt} />
+          <img
+            onClick={() => setIntroPage(!introPage)}
+            src={btnImg}
+            alt={btnAlt}
+          />
         </div>
       </section>
     </>
--- foodzilla/src/app/page.jsx
+++ foodzilla/src/app/page.jsx
@@ -8,17 +8,12 @@ import Navbar from "../../components/navbar";
 import Footer from "../../components/footer";
 
 export default function Home({ title, imgSrc }) {
-  const [starterPage, setStarterPage] = useState(false);
-
   return (
     <>
       <Navbar />
       <WelcomePage
         logo={"/foodzilla_logo.svg"}
         logoAlt={"Foodzilla logo"}
-        welcomeText={
-          "Your cooking is worth saving. Get organized with foodzilla"
-        }
         btnImg={"/forward.svg"}
       />
       <Footer />
